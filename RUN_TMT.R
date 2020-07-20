7d792deeb2298f010121705b62a930a17d178bcc
Natalie Clark <nmclark2@iastate.edu>
2020-07-20 12:13:34 -0500

    Update RUN_TMT.R

--- RUN_TMT.R
+++ RUN_TMT.R
@@ -1,6 +1,5 @@
 #run all code in this file to start the TMT analysis pipeline
 
 library(shiny)
-setwd("D:/OneDrive/Documents/Walley Lab Postdoc/TMT analysis") #change to your own directory that contains the pipeline!
 source("TMT_pseq_pipeline.R")
 runApp('app.R')
