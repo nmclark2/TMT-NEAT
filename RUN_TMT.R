81a30943fc55fe4c9d9e61381324518d6846ea92
Natalie Clark <nclark@broadinstitute.org>
2022-03-08 15:41:22 -0500

    Add files via upload

--- RUN_TMT.R
+++ RUN_TMT.R
@@ -1,7 +1,7 @@
 #run all code in this file to start the TMT analysis pipeline
 
 #check that all packages are installed, and load them
-for (package in c('openxlsx', 'plyr','dplyr','PoissonSeq','shiny')) {
+for (package in c('openxlsx', 'plyr','dplyr','shiny')) {
   if (!require(package, character.only=T, quietly=T)) {
     install.packages(package)
     library(package, character.only=T)
@@ -14,7 +14,9 @@ if (!require('ggbiplot',quietly=T)) {
   install.packages('devtools')
   library(devtools)
   install_github("vqv/ggbiplot")
+  install_github("cran/PoissonSeq")
   library('ggbiplot')
+  library('PoissonSeq')
 }else{
   library('ggbiplot')
 }
